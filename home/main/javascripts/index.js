fd9783d2940cb56073dadc2f12fe803cf526aa86
majinmaru <gotch11@naver.com>
2019-08-09 00:17:35 +0900

    190809
    main contents

--- home/main/javascripts/index.js
+++ home/main/javascripts/index.js
@@ -3,6 +3,33 @@ $(document).ready(function(){
 		zindex: "3"
 	});
 
+	$(".first-context__left-tab").eq(0).addClass('current');
+	$(".first-context__left-tab").eq(0).find(".left-tab__btn-img").attr('src', './images/vehicles-btn-active.png');
+
+	$(".first-context__left-tab").hover(function(){
+		var getIndex = $(this).index();
+		if($(this).hasClass('current')){
+			return false;
+		} else {
+			$(".first-context__left-tab").eq(getIndex).addClass('hover');
+		}
+	}, function(){
+		$(".first-context__left-tab.hover").removeClass('hover');
+	});
+
+	$(".left-tab__btn").click(function(e){
+		e.preventDefault();
+		var getIndex = $(this).parent().index();
+		if($(".first-context__left-tab").eq(getIndex).hasClass('current')){
+			return false;
+		} else {
+			$(".first-context__left-tab").eq(getIndex).addClass('current');
+			$(".first-context__left-tab").eq(getIndex).find(".left-tab__btn-img").attr('src', './images/vehicles-btn-active.png');
+			$(".first-context__left-tab").eq(getIndex).siblings().removeClass('current');
+			$(".first-context__left-tab").eq(getIndex).siblings().find(".left-tab__btn-img").attr('src', './images/vehicles-btn-hover.png');
+		}
+	});
+
 	$(".third-right__category-container").mCustomScrollbar({
 	    axis:"x", // horizontal scrollbar
 	    theme: "minimal",
