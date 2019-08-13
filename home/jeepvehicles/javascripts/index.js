04aa11097b096361f1cf3d121bdca768e3959aac
majinmaru <gotch11@naver.com>
2019-08-14 01:50:02 +0900

    190814
    model done

--- home/jeepvehicles/javascripts/index.js
+++ home/jeepvehicles/javascripts/index.js
@@ -2,4 +2,69 @@ $(document).ready(function(){
 	$("html").niceScroll({
 		zindex: "3"
 	});
+	$(".more-btn").hover(function(){
+		if($(".more-title").hasClass('visible')){
+			return false;
+		} else {
+			$(".more-title").css({
+				'left' : '0',
+				'opacity' : '1'
+			});
+		}
+	}, function(){
+		if($(".more-title").hasClass('visible')){
+			return false;
+		} else {
+			$(".more-title").css({
+				'left' : '-16.5rem',
+				'opacity' : '0'
+			});
+		}
+	})
+	$(".more-btn").click(function(e){
+		e.preventDefault();
+		if($(".more-title").hasClass('visible')){
+			$(".more-title").css({
+				'left' : '-16.5rem',
+				'opacity' : '0'
+			});
+			$(".more-title").removeClass('visible');
+			$(".more-btn").find("img").removeClass('rotate');
+		} else {
+			$(".more-title").css({
+				'left' : '0',
+				'opacity' : '1'
+			});
+			$(".more-title").addClass('visible');
+			$(".more-btn").find("img").addClass('rotate');
+		}
+	});
+	$(".more-title__text").click(function(e){
+		e.preventDefault();
+		$(".exterior-popup").css('display', 'block');
+		$(".exterior-popup").addClass('on');
+		/*
+		$(this).on('scroll touchmove mousewheel', function(event) {
+		  event.preventDefault();
+		  event.stopPropagation();
+		  return false;
+		});
+		*/
+	})
+	$(".popup-content__close").click(function(e){
+		e.preventDefault();
+		$(".exterior-popup").css('display', 'none');
+		$(".exterior-popup").removeClass('on');
+	});
+	/*
+	$('html').click(function(e) { 
+		if(!$(e.target).hasClass('visible')) { 
+			$(".more-title").css({
+				'left' : '-16.5rem',
+				'opacity' : '0'
+			});
+			$(".more-title").removeClass('visible');
+		}
+	});
+	*/
 });
--- home/main/javascripts/index.js
+++ home/main/javascripts/index.js
@@ -39,4 +39,64 @@ $(document).ready(function(){
 	    	normalizeDelta: true
 	    }
 	});
+
+	var slideBg = $(".bg-slide").find(".bg-slide__img");
+	var slideImg = $(".slide-img").find(".slide-img__img");
+	var slideText = $(".slide-text__sub").find(".slide-text__sub-text");
+	var slideNum = $(".slide-num__current");
+	var dirBtn = $(".slide-dir__btn");
+
+	slideBg.eq(0).css('display', 'inline-block');
+	slideBg.eq(0).addClass('current');
+	slideImg.eq(0).css('display', 'inline-block');
+	slideImg.eq(0).addClass('current');
+	slideText.eq(0).css('display', 'inline-block');
+	slideText.eq(0).addClass('current');
+
+	function runSlideBg(nextSlideIndex, currentSlideBg){
+		var nextItem = slideBg.eq(nextSlideIndex);
+		currentSlideBg.removeClass('current');
+		currentSlideBg.css('display', 'none');
+		nextItem.css('display', 'inline-block');
+		nextItem.addClass('current');
+	}
+	function runSlideImg(nextSlideIndex, currentSlideImg){
+		var nextItem = slideImg.eq(nextSlideIndex);
+		currentSlideImg.removeClass('current');
+		currentSlideImg.css('display', 'none');
+		nextItem.css('display', 'inline-block');
+		nextItem.addClass('current');
+	}
+	function runSlideText(nextSlideIndex, currentSlideText){
+		var nextItem = slideText.eq(nextSlideIndex);
+		currentSlideText.removeClass('current');
+		currentSlideText.css('display', 'none');
+		nextItem.css('display', 'inline-block');
+		nextItem.addClass('current');
+	}
+
+	dirBtn.on('click', function(e){
+		e.preventDefault();
+		var currentSlideBg = slideBg.filter(".current");
+		var currentSlideIndex = currentSlideBg.index();
+		var nextSlideIndex;
+		var nextNum;
+		var currentSlideImg = slideImg.filter(".current");
+		var currentSlideText = slideText.filter(".current");
+		if($(this).hasClass('left')){
+			nextSlideIndex = currentSlideBg.prev().index();
+			currentSlideIndex = nextSlideIndex >= 0 ? nextSlideIndex : slideBg.last().index();
+			nextNum = currentSlideIndex + 1
+			slideNum.text('0' + nextNum);	
+		}
+		if($(this).hasClass('right')){
+			nextSlideIndex = currentSlideBg.next().index();
+			currentSlideIndex = nextSlideIndex >= 0 ? nextSlideIndex : slideBg.first().index();
+			nextNum = currentSlideIndex + 1
+			slideNum.text('0' + nextNum);		
+		}
+		runSlideBg(currentSlideIndex, currentSlideBg);
+		runSlideImg(currentSlideIndex, currentSlideImg);
+		runSlideText(currentSlideIndex, currentSlideText);
+	})
 });
