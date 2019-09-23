49e9d69611dc064e844c92e649dba263cc904e74
majinmaru <gotch11@naver.com>
2019-09-23 14:01:31 +0900

    190923

--- home/jeepshowroom/javascripts/index.js
+++ home/jeepshowroom/javascripts/index.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 	$("html").niceScroll({
 		zindex: "3"
 	});
-	/*
+	
 	$(".title-right__popup").hover(function(){
 		$(".title-right__plus").addClass('hover');
 	}, function(){
 		$(".title-right__plus").removeClass('hover');
 	})
-	*/
+	
 });
